ffbd523dfe78ae1fecf95fe36f9ffb9ea1ed73f0
bararchy <bar.hofesh@safe-t.com>
2015-06-08 17:20:43 +0300

    Fixed read for using arrUint

--- aeon-client.cr
+++ aeon-client.cr
@@ -7,7 +7,7 @@ class AeonClient
     alter_aeon_port = 3000
     connection = TCPSocket.new(alter_aeon_server, alter_aeon_port)
     spawn autoreader(connection)
-    spawn writer(connection)
+    # spawn writer(connection)
     Signal::INT.trap do
       connection << "quit" if connection
       sleep 1
@@ -15,30 +15,32 @@ class AeonClient
       puts "Exiting.."
       exit 0
     end
-    while connection
-      sleep 1
-    end
-
-  end
-
-  def self.writer(connection)
-    while connection
+    unless connection.closed?
       begin
         buff = gets
         connection << buff.to_s if connection
       rescue e : Exception
-        sleep 0.5
+        puts e
       end
     end
   end
 
   def self.autoreader(connection)
-    loop do
+    unless connection.closed?
       begin
-        while(buff = connection.read_nonblock(4096))
-          print buff if buff
+        buf :: UInt8[4096]
+        len = connection.read(buf.to_slice)
+        if len > 0
+          data = String.new(buf.buffer, len)
+          print data if !data.empty?
         end
+      rescue e : IO::EOFError
+        puts "EOFError"
+        sleep 0.5
       rescue e : Exception
+        p e
+        puts e.message
+        puts e.class
         sleep 0.5
       end
     end
