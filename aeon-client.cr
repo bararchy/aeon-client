34169a8f7a15d9a0c7a3c5156992222940d72625
bararchy <bar.hofesh@safe-t.com>
2015-06-09 10:09:43 +0300

    Latest

--- aeon-client.cr
+++ aeon-client.cr
@@ -7,7 +7,6 @@ class AeonClient
     alter_aeon_port = 3000
     connection = TCPSocket.new(alter_aeon_server, alter_aeon_port)
     spawn autoreader(connection)
-    # spawn writer(connection)
     Signal::INT.trap do
       connection << "quit" if connection
       sleep 1
@@ -19,6 +18,7 @@ class AeonClient
       begin
         buff = gets
         connection << buff.to_s if connection
+        break if connection.closed?
       rescue e : Exception
         puts e
       end
@@ -28,24 +28,20 @@ class AeonClient
   def self.autoreader(connection)
     loop do
       begin
+        break if connection.closed?
         buf :: UInt8[4096]
         len = connection.read(buf.to_slice)
         if len > 0
           data = String.new(buf.buffer, len)
           print data if !data.empty?
+        else
+          sleep 0.5
         end
-      rescue e : IO::EOFError
-        puts "EOFError"
-        sleep 0.5
       rescue e : Exception
-        p e
-        puts e.message
-        puts e.class
-        sleep 0.5
+        puts e
       end
     end
   end
-
 end
 
 AeonClient.main
